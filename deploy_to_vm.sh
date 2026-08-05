40954a1527e87f3143d212762ceb539f7c2733db
D2SD <samueloso21@gmail.com>
2026-08-05 14:49:01 -0400

    add sa key to deploy

--- deploy_to_vm.sh
+++ deploy_to_vm.sh
@@ -2,6 +2,9 @@
 
 export $(cat .env.gcp | xargs)
 
+echo "Docker Login"
+cat ./serviceAccountKey.json | base64 -d -w 0 | docker login -u _json_key --password-stdin https://${GCP_ARTIFACT_REGISTRY_LOCATION}-docker.pkg.dev
+
 echo "Compose pull"
 docker compose --env-file .env.gcp pull
 
